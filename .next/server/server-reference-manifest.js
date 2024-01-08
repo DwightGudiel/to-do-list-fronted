be8e1ec9625e03df8719db2529adb58b7107c6b7
Daniel Bor <danielebs175@gmail.com>
2024-01-08 08:07:16 -0600

    build 05/01

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"oK4Hhl+H6Mk2DwM2JdZcbNgEbptvUNf8PKKkLrdc0v0=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"aoW3K1U0VWMal81p9BgJNHFPZM0sZBrUWzsn9Lk1/Gw=\"}"
